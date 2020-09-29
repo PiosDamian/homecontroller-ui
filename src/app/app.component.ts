fcd55fd7b6189d6d3d504d1b57dde9e475099d62
Damian Pioś <damian.pios@infinite.pl>
2020-09-29 15:28:22 +0200

    alert styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { CommunicationService } from './core/services/communication/communication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(communication: CommunicationService) {}
-}
+export class AppComponent {}
