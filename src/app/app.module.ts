5a5a6ba04c85fae6a2bebd5e52baf51ebf48e1a6
Damian Pioś <damian.pios@infinite.pl>
2020-09-29 15:20:19 +0200

    service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
